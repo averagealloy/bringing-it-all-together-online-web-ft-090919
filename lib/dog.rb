377a3dd64a70cfbbdd119f18c98a0db1267b3312
Mike McTighe <m.mctighe10@gmail.com>
2019-10-14 01:21:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,13 +73,13 @@ end
       dog = DB[:conn].execute(sql, name, breed).first
 
       if dog
-   new_dog = self.new_from_db(dog)
- else
-   new_dog = self.create({:name => name, :breed => breed})
- end
- new_dog
-end
-
-
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
     end
+
+
+    
 end
