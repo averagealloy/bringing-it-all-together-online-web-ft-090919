3626770daa1bb505772e40dca9d454d131cb5d26
Mike McTighe <m.mctighe10@gmail.com>
2019-10-14 00:43:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,7 +37,7 @@ attr_accessor :name, :breed, :id
 
     SQL
     DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT ")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
 
 end
