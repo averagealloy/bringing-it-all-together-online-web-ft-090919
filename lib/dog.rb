a44b0af23617d55609b866691f223c3af78adba2
Mike McTighe <m.mctighe10@gmail.com>
2019-10-14 00:09:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,6 +7,24 @@ attr_accessor :name, :breed, :id
     @id = id
     @name = name
     @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+
+    )
+
+    SQL
+    DB[:conn].execute(sql)
 
   end
+
+  def self.drop_table
+    
+  end
+
 end
