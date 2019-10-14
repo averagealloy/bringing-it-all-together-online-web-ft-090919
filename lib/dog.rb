17c0d6627506ffae3d1a52c543297a26c41509e7
Mike McTighe <m.mctighe10@gmail.com>
2019-10-14 01:35:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,7 +82,7 @@ end
 
     def self.find_by_name(name)
       sql = <<-SQL
-      SELECT * FROM dogs WHERE name ?
+      SELECT * FROM dogs WHERE name = ?
       SQL
       DB[:conn].execute(sql, name).map do |row|
         self.new_from_db(row)
